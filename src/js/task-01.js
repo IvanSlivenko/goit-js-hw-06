c872dde2de050acffd2288a5c4b0009f7a1ed4fd
IvanSlivenko <ivan8822@ulr.net>
2023-01-03 21:28:00 +0200

    etap-03-01-2022-21-27

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,18 @@
-const ArrCategoryy = document.getElementById('categories');
-console.log(ArrCategoryy);
+
+const arrCategoryy = document.querySelectorAll('.item');
+console.log(`Number of categories: ${arrCategoryy.length}`);
+
+
+
+
+const masuv = document.querySelectorAll('.item')
+
+
+const message = masuv.forEach((element) => { 
+ 
+    console.log('Ctegory:', element.children[0].textContent);
+    console.log('Elements:',element.children[1].children.length);
+})
+
+
+
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+//Отримаємо доступ до ul
+const ingridientsList = document.querySelector('#ingredients');
+// ingridientsList.textContent = 'ingridients';
+
+//створимо масив-контейнер  
+const container = [];
+
+//робот для створення елементів
+  const masuv=ingredients.map((elem) => {
+    const element = document.createElement('li');
+    element.textContent = elem;
+    element.classList.add('.item');
+    container.push(element);
+    
+  })
+//pозмістимо елементи з контейнера 
+ingridientsList.append(...container);
+
