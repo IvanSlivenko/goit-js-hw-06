ef4524796070e1299c72b6609adee0ba71ba4b98
IvanSlivenko <ivan8822@ulr.net>
2023-01-05 12:40:33 +0200

    etap-05-01-2023-12-40

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -2,18 +2,31 @@
 
 const forms = document.querySelector('.login-form');
 
-
-const inputEmail = forms.children[0].children[0];
-const inputPassword = forms.children[1].children[0];
-const btn = forms.children[2];
-
 forms.addEventListener('submit', submitForms);
 
 function submitForms(event) {
     event.preventDefault();
-console.log(event);
-    
-    
+    const transytMasyv = {
+        email: '',
+        password:'',
+    }
+    // console.log(event.currentTarget);
+
+    if (event.currentTarget.elements.email.value === '' || event.currentTarget.elements.password.value === '') {
+        
+        window.alert('Не всі поля форми  містять данні (перевірте email та password)');
+    } else { 
+    // console.dir(event.currentTarget.elements.email.value);
+    //     console.dir(event.currentTarget.elements.password.value); 
+        
+        transytMasyv.email = event.currentTarget.elements.email.value;
+        transytMasyv.password = event.currentTarget.elements.password.value;
+
+        console.log(transytMasyv);
+    }
+
+
+
 }
 
 //Модуль 7. Занятие 14. События (17.09)
