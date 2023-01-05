4e60e1703a60389c39ff0b2137533effc5f938e5
IvanSlivenko <ivan8822@ulr.net>
2023-01-05 13:56:04 +0200

    etap-05-01-2023-13-55

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,21 @@
+const refs = {
+  bntChangeColor: document.querySelector('.change-color'),
+  SpanColor: document.querySelector('.color'),
+    
+}
+
+refs.bntChangeColor.addEventListener('click',ChangeColorBody)
+
+function ChangeColorBody() {
+  // console.log(document.body);
+  // console.dir(document.body);
+  document.body.style.background = getRandomHexColor();
+  // console.log(document.body.style.background);
+  refs.SpanColor.textContent = document.body.style.background
+  refs.SpanColor.style.color = getRandomHexColor();
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
